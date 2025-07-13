09dcdc335898da54192162b8a91fe724c525f3ca
gajanan <gajanankale97@gmail.com>
2025-07-13 18:27:11 +0530

    Operattors gits commit

---
+++ src/operators/Lab0036_opratorsassignement.java
@@ -0,0 +1,15 @@
+package operators;
+
+public class Lab0036_opratorsassignement
+{
+    public static void main(String[] args) {
+        int age=65;
+        //int --data type
+        //age - ifrntifier
+        //65-value (literal)
+        //=- assignement operatrors
+
+
+    }
+
+}
